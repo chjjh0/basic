0f3212c4264190cf36c28b687f3c3350bcadbfc5
gnssp <chjjh@naver.com>
2018-06-03 13:58:58 +0900

    2

--- src/step1/Bmi.java
+++ src/step1/Bmi.java
@@ -1,60 +1,67 @@
 package step1;
-import java.util.Scanner;
-/**
--주제 : BMI 지수 구하기
--내용 : 몸무게와 키를 입력받고 연산하여 BMI 지수를 출력
--입력 : 이름, 몸무게, 키
--출력 : 이름, BMI 지수, 정상/비정상 여부
--기능 : 종료, 재시작
-**/
-public class Bmi {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		while(true){
-            double height = 0.0, weight = 0.0, bmi = 0.0;
-            String name = "", result = "";
-            int flag = 0;
-                        
-            System.out.print("이름 ");
-            name = scan.nextLine();
-        
-            System.out.print("몸무게 ");
-            weight = scan.nextDouble();
-        
-            System.out.print("키 ");
-            height = scan.nextDouble();
-            height *= 0.01;
-        
-        bmi = (weight / (height * height));
 
-	    if(bmi < 20){ result = "저체중";
-	    } else if(bmi >= 20 && bmi <= 24) {
-    		result = "정상체중";
-    	} else if(bmi >= 25 && bmi <= 30) {
-	    	result = "경도비만";
-	    } else 
-		    result = "비만";	
-       
-        System.out.printf(name + "님은 BMI 지수" + " %.1f 으로 [" + result + "] 입니다 \n", bmi);
-    
-        System.out.println("0 : 종료 1 : 재시작");
-        flag = scan.nextInt();
-        scan.nextLine();                
-        switch(flag){
-            case 0 : 
-                System.out.println("종료");
-                return;
-            case 1 : 
-                System.out.println("재시작");
-                continue;
-            default :
-                System.out.println("잘못 입력했습니다");
-                continue;
-        }
-        
-    }
+import java.util.Scanner;
+
+/**
+ * -주제 : BMI 지수 구하기 
+ * -내용 : '몸무게'와 '키'를 입력받고 연산하여 BMI 지수를 출력 
+ * -입력 : 이름, 몸무게, 키 
+ * -출력 : 이름, BMI 지수, 정상/비정상 여부 
+ * -기능 : 실행, 종료
+ **/
+public class Bmi {
+
+	public static String calc(String[] params) {
+		double height = Integer.parseInt(params[1]) * 0.01;
+		double weight = Integer.parseInt(params[0]);
+		
+		String bmi = String.valueOf((int)((weight / (height * height))));
+		return bmi;
+	}
 	
+	public static String grade(int param) {
+		String result = "";
+		
+		if (param < 20) {
+			result = "저체중";
+		} else if (param >= 20 && param <= 24) {
+			result = "정상체중";
+		} else if (param >= 25 && param <= 30) {
+			result = "경도비만";
+		} else
+			result = "비만";
+		return result; 
 	}
 
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("[0] : 종료 [1] 실행");
+			switch(scan.nextInt()) {
+				case 0: return;
+				case 1: 
+			
+					String[] category = { "몸무게", "키" };
+					String name = "";
+					String[] temp = new String[category.length];
+					String[] result = new String[3];
+
+					System.out.println("이름을 입력하세요");
+					name = scan.next();
+					
+					for (int i = 0; i < category.length; i++) {
+						System.out.printf("[ %s ]를 입력하세요", category[i]);
+						temp[i] = scan.next();
+					}
+					result[0] = calc(temp);
+					result[1] = grade(Integer.parseInt(result[0])); 
+					
+					System.out.printf("%s 님의 BMI 지수는  %s 로 [ %s ] 입니다 \n",
+										name, result[0], result[1]);
+					break;
+				default : System.out.println("ERROR");
+			}
+		}
+	}
 }
--- src/step2/splitTest.java
+++ src/step2/splitTest.java
@@ -3,13 +3,11 @@ package step2;
 import java.util.Arrays;
 
 public class splitTest {
-public static void main(String[] args) {
-	String str1 = "1. 2. 3";
-	System.out.println(str1);
-	String str2[] = str1.split("\\.");
-	System.out.println(Arrays.toString(str2));
-	System.out.printf("%s%s%s", str2[0], str2[1], str2[2]);
-	
-	
-}
+	public static void main(String[] args) {
+		String str1 = "1. 2. 3";
+		System.out.println(str1);
+		String str2[] = str1.split("\\.");
+		System.out.println(Arrays.toString(str2));
+		System.out.printf("%s %s %s", str2[0], str2[1], str2[2]);
+	}
 }
