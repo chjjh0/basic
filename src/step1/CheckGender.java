6e8115178216f42b337a4b9c4801d932acb44c21
gnssp <chjjh@naver.com>
2018-06-03 14:59:28 +0900

    3

--- src/step1/CheckGender.java
+++ src/step1/CheckGender.java
@@ -8,42 +8,37 @@ package step1;
 **/
 import java.util.Scanner;
 public class CheckGender {
+	public static String check(String param) {
+		String result="";
+		char ch = param.charAt(7);
+		switch(ch){
+        	case '1' : case '3' : result = "남자"; break;
+        	case '2' : case '4' : result = "여자"; break;
+        	case '5' : case '6' : result = "외국인"; break;
+        	default : result = "ERROR"; break;}
+		return result;
+}
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        
         while(true){
-            
-        	String name = "", idNumber = "", gender = "", error = "ERROR", result = "";
-        	char ch = 0;
-        
-        	scan.nextLine();
-        	System.out.print("이름");
-        	name = scan.nextLine();
-        	
-        System.out.print("주민번호");
-        idNumber = scan.nextLine();
-        
-        ch = idNumber.charAt(7);
-        
-        switch(ch){
-            case '1' : case '3' : gender = "남자"; break;
-            case '2' : case '4' : gender = "여자"; break;
-            case '5' : case '6' : gender = "외국인"; break;
-            default : result = error; }
-        //System.out.println(result);
-        
-        if(!result.equals(error)){
-            result = "이름 : " + name + " 성별 : "+ gender + "입니다";
-        } else {
-            result = result;            
-        }
-        System.out.println(result);
-        System.out.println("0 : 종료 1 : 재시작");
-            switch(scan.nextInt()){
-                case 0 : return; 
-                case 1 : continue;
-                default : System.out.println(error); return;
+            System.out.println("[0] 종료 [1] 실행");
+            switch(scan.nextInt()) {
+            case 0: return;
+            case 1:
+	        	String category[] = {"이름", "주민번호"};
+	        	String[] temp = new String[2];
+	        	String[] result = new String[2];
+	        	
+	        	for(int i=0;i<category.length;i++) {
+	        		System.out.printf("[ %s ]을 입력하세요", category[i]);
+	        		temp[i] = scan.next();
+	        	}
+	        	result[0] = temp[0];
+	        	result[1] = check(temp[1]);
+	        	System.out.printf("이름: %s 성별: %s\n", result[0], result[1]);
+	        	break;
+	        default : System.out.println("ERROR"); break;
+            }
         }
     }
-}
 }
